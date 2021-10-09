3abe94525fad318e9c20abbb6b20fc7bea4e164e
Natalya Posokhova <natalya.posokhova@gmail.com>
2021-10-09 15:16:54 +0500

    initial

---
+++ YandexMarketTests.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace SeleniumYandexMarketTests
+{
+    public class YandexMarketTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+    }
+}

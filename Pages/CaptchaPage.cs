31335828a204e32ccbffc8a25682e6dfb269ba23
Natalya Posokhova <natalya.posokhova@gmail.com>
2021-10-10 21:18:16 +0500

    Add captcha page.

---
+++ Pages/CaptchaPage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumYandexMarketTests.Pages
+{
+    /// <summary>
+    /// Captcha, appears when trying to load main yandex page by selenium.
+    /// </summary>
+    internal class CaptchaPage
+    {
+        private IWebDriver _driver;
+        public CaptchaPage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = ".CheckboxCaptcha-Button")]
+        [CacheLookup]
+        public IWebElement Captcha { get; set; }
+    }
+}

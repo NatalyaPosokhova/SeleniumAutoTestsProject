a7688b0a461f29eaca6c849eb5c10b0adf504a84
Natalya Posokhova <natalya.posokhova@gmail.com>
2021-10-10 21:11:16 +0500

    Add yandex page

---
+++ Pages/YandexMarketHomePage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace SeleniumYandexMarketTests.Pages
+{
+    /// <summary>
+    /// Yandex Market Main Page
+    /// </summary>
+    internal class YandexMarketHomePage
+    {
+        private IWebDriver _driver;
+        public YandexMarketHomePage(IWebDriver driver)
+        {
+            _driver = driver;
+            PageFactory.InitElements(_driver, this);
+        }
+        
+        [FindsBy(How = How.XPath, Using = "//*[@data-apiary-widget-name='@MarketNode/HeaderCatalogEntrypoint']/*/*[@type='button']")]
+        [CacheLookup]
+        public IWebElement BurgerMenu { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@data-zone-name='category-link']/*/*[@class='_1UCDW' and text()='Компьютеры']")]
+        [CacheLookup]
+        public IWebElement ComputersSection { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@data-autotest-id='subItems']/*/*/*[text()='Ноутбуки']")]
+        [CacheLookup]
+        public IWebElement LaptopsSection { get; set; }
+
+    }
+}
--- YandexMarketTests.cs
+++ YandexMarketTests.cs
@@ -1,13 +1,31 @@
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace SeleniumYandexMarketTests
 {
-    public class YandexMarketTests
+    internal class YandexMarketTests
     {
+        private IWebDriver _driver;
+        private string _baseURL;
+
         [SetUp]
         public void Setup()
         {
+            _driver = new ChromeDriver();
+            _baseURL = "https://market.yandex.ru/";
         }
 
+        [TearDown]
+        public void CleanUp()
+        {
+            
+        }
+
+        [Test]
+        public void SearchDefiniteCostLaptopsOnYandexMarket()
+        {
+            
+        }       
     }
 }
